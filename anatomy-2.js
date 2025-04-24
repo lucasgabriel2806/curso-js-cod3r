deac92cc9d5f846d9ed8591ed66fcddfeef74ee7
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-04-24 12:26:38 -0300

    Anatomia da Função #02

---
+++ anatomy-2.js
@@ -0,0 +1,23 @@
+// Anonymous function
+/** Uma função anonima só funciona dentro de parenteses */
+(function (a, b, c) {
+    return a + b + c;
+})
+
+// Function expression
+/** Com isso é possível atribuir ao valor de uma var ou const o valor de uma função */
+var sum = function (a, b) {
+    return a + b;
+}
+
+var result = sum(1, 2);
+console.log(result);
+
+var anotherSum = sum;
+console.log(anotherSum(5, 9));
+
+let x = 3;
+console.log(x);
+
+x = sum;
+console.log(x(11, 16))
