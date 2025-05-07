667c439f3537829dc8c55a42851819650ef49e4e
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-05-07 07:14:13 -0300

    Higher-Order Function #02

--- js-functions/functional/first-class-function.js
+++ js-functions/functional/first-class-function.js
@@ -2,7 +2,7 @@
  * A programming language is said to have 
  * First-class functions when functions in
  * that language are treated like any other
- * variable.
+ * variable
  */
 
 const add = function(a, b) {
