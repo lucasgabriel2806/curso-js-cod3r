74333df850ff1e13244e37a98c8be77d8dc10677
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-06-20 09:16:40 -0300

    Atualizando Função #02

--- js-functions/funcoes/basico_2.js
+++ js-functions/funcoes/basico_2.js
@@ -6,14 +6,39 @@ const boaTarde = function() {
     console.log('Boa tarde!');
 }
 
-// Passar uma função como parametro pra outra função
-// 7:09
+// 1) Passar uma função como parametro pra outra função
 function executarQualquerCoisa(fn) {
+
     if (typeof fn === 'function') {
-        fn()
+
+        fn();
+
     }
+
 }
 
 executarQualquerCoisa(3);
+
 executarQualquerCoisa(bomDia);
+
 executarQualquerCoisa(boaTarde);
+
+// 2) Retornar uma função a partir de outra função
+
+function potencia(base) {
+
+    return function(exp) {
+
+        return Math.pow(base, exp);
+
+    }
+
+}
+
+const potenciaDe2 = potencia(2);
+
+console.log(potenciaDe2(8));
+
+const pot34 = potencia(3)(4);
+
+console.log(pot34);
