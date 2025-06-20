501c24ce81874f20f19e9f76aca3bfc61222acb2
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-06-20 16:32:15 -0300

    Função Map #02

--- js-functions/funcoes/callback_3.js
+++ js-functions/funcoes/callback_3.js
@@ -11,3 +11,19 @@ const primeiraLetra = texto => texto[0];
 const letras = nomes.map(primeiraLetra);
 
 console.log(nomes, letras);
+
+const carrinho = [
+    { nome: 'Caneta', qtde: 10, preco: 7.99 },
+    { nome: 'Impressora', qtde: 0, preco: 649.50 },
+    { nome: 'Caderno', qtde: 4, preco: 27.10 },
+    { nome: 'Lapis', qtde: 3, preco: 5.82 },
+    { nome: 'Tesoura', qtde: 1, preco: 19.20 },
+];
+
+const nomesCarrinho = carrinho.map(el => el.nome);
+
+console.log(nomesCarrinho);
+
+const totalCarrinho = carrinho.map(el => el.qtde * el.preco);
+
+console.log(totalCarrinho);
