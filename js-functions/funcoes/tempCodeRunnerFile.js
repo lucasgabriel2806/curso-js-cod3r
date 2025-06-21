ab1954156ad5a047c596522f1e64b9e7b0be3eb6
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-06-21 14:54:57 -0300

    Função Filter #01

--- js-functions/funcoes/tempCodeRunnerFile.js
+++ js-functions/funcoes/tempCodeRunnerFile.js
@@ -1,7 +1,1 @@
-const nomesCarrinho = carrinho.map(el => el.nome);
-
-// console.log(nomesCarrinho);
-
-// const totalCarrinho = carrinho.map(el => el.qtde * el.preco);
-
-// console.log(totalCarrinho);
+qtdeMaiorQueZero
