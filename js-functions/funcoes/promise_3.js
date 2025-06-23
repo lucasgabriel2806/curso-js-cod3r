f0dfd64c132787a70133a89e4231c0e91567e86c
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-06-23 10:21:49 -0300

    Promise #03

---
+++ js-functions/funcoes/promise_3.js
@@ -0,0 +1,27 @@
+function gerarNumerosEntre(min, max) {
+
+    /** Correção caso digite a ordem errada */
+    if(min > max) {
+
+        /** Destructuring */
+        [max, min] = [min, max];
+                    /** Array */
+
+    }
+
+    return new Promise(resolve => {
+
+        const fator = max - min + 1;
+
+        const aleatorio = parseInt(Math.random() * fator) + min;
+
+        resolve(aleatorio);
+
+    });
+
+}
+
+gerarNumerosEntre(1, 60)
+    .then(num => num * 10)
+    .then(numX10 => `O número gerado foi ${numX10}`)
+    .then(console.log);
