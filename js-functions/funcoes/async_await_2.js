5ab1e4c67ec9675624343e5d342d833d9c605605
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-06-24 16:34:34 -0300

    Async / Await #02

--- js-functions/funcoes/async_await_2.js
+++ js-functions/funcoes/async_await_2.js
@@ -29,6 +29,40 @@ function gerarNumerosEntre(min, max, numerosProibidos) {
 
 }
 
-gerarNumerosEntre(1, 5, [1, 2, 4])
+async function gerarMegaSena(qtdeNumeros, tentativas = 1) {
+
+    try {
+
+        const numeros = [];
+
+        for(let _ of Array(qtdeNumero).fill()) {
+
+            numeros.push(await gerarNumerosEntre(1, 60, numeros));
+
+        }
+
+        return numeros;
+
+    } catch(e) {
+
+        if(tentativas > 10) {
+
+            throw 'Não deu certo!';
+
+        } else {
+
+            gerarMegaSena(qtdeNumeros, tentativas + 1);
+
+        }
+
+    }
+
+}
+
+gerarMegaSena(15)
     .then(console.log)
     .catch(console.log);
+
+// gerarNumerosEntre(1, 5, [1, 2, 4])
+//     .then(console.log)
+//     .catch(console.log);
