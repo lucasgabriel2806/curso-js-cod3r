ac5df3fccdf00cd1e7ad0bdbae7d3fb2d6b707bf
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-06-25 13:27:56 -0300

    Challenge

--- js-functions/foundation/challenge.js
+++ js-functions/foundation/challenge.js
@@ -1,19 +1,35 @@
 // Create a range function
-// range(5) -> [1, 2, 3, 4, 5];
-// range(6, 11) -> [6, 7, 8, 9, 10, 11];
-// range(10, 19, 2) -> [10, 12, 14, 16, 18];
-// range(6, 2) -> [6, 5, 4, 3, 2];
-// range(8, -3, 4); -> [8, 4, 0];
+function range(a, b, s = 1) {
 
-function rangeNumber(a, b = 0, c = 0) {
+    const n1 = b === undefined ? 1 : a;
 
-    let i = 1;
+    const n2 = b === undefined ? a : b;
 
-    while (i <= 5) {
-        console.log(i);
-        i++;
+    const step = n1 <= n2 ? Math.abs(s) : -Math.abs(s);
+
+    const nums = [];
+
+    for(let i = n1; n1 <= n2 ? i <= n2 : i >= n2; i += step) {
+
+        nums.push(i);
+
     }
+
+    return nums;
 
 }
 
-rangeNumber(5);
+// range(5) -> [1, 2, 3, 4, 5];
+console.log(range(5));
+
+// range(6, 11) -> [6, 7, 8, 9, 10, 11];
+console.log(range(6, 11));
+
+// range(10, 19, 2) -> [10, 12, 14, 16, 18];
+console.log(range(10, 19, 2));
+
+// range(6, 2) -> [6, 5, 4, 3, 2];
+console.log(range(6, 2));
+
+// range(8, -3, 4); -> [8, 4, 0];
+console.log(range(8, -3, 4));
